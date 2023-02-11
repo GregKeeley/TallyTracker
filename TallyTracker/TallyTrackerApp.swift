11b3206ac3d0056f8c94d638c58e880e5bbaba8e
GregKeeley <darkscuall@yahoo.com>
2023-02-11 10:06:55 -0500

    Initial Commit

---
+++ TallyTracker/TallyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallyTrackerApp.swift
+//  TallyTracker
+//
+//  Created by Gregory Keeley on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TallyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
