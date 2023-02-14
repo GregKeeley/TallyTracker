2ea56cc4fd1f8e2bd1e7ff79e3c2dc0a6cd30207
GregKeeley <darkscuall@yahoo.com>
2023-02-14 17:33:20 -0500

    Played around with the style of the name on the TallyButton view, before commenting it back out to wait for an informed design.

--- TallyTracker/TallyButtonView.swift
+++ TallyTracker/TallyButtonView.swift
@@ -14,15 +14,26 @@ struct TallyButtonView: View {
     @State var playerName: String
     
     var body: some View {
+        GeometryReader { geo in
             ZStack {
-//                VStack {
-//                    Text(playerName)
-//                        .font(.system(size: 32, weight: .thin))
-////                        .foregroundColor(color.opacity(0.8))
-//                        .background(color.opacity(0.8))
-//                        .padding()
-//                    Spacer()
-//                }
+                VStack {
+                    
+                    HStack {
+                        ZStack {
+//                            RoundedRectangle(cornerRadius: 8)
+//                                .foregroundColor(color.opacity(0.8))
+//                                .frame(height: 48)
+//                            Text(playerName)
+//                                .font(.system(size: 32, weight: .regular))
+//                                .frame(maxWidth: .infinity)
+//                                .background(color.opacity(0.8).colorInvert())
+//                                .colorInvert()
+//                                .padding()
+                        }
+                        Spacer()
+                    }
+                    Spacer()
+                }
                 RoundedRectangle(cornerRadius: 8)
                     .stroke()
                     .foregroundColor(color.opacity(0.8))
@@ -30,7 +41,7 @@ struct TallyButtonView: View {
                     .font(.system(size: 148, weight: .bold))
                     .minimumScaleFactor(0.5)
                     .foregroundColor(color.opacity(0.8))
-                    
+                
             }
             .rotationEffect(orientation == .portrait ? .degrees(0) : (UIDevice.current.orientation.rawValue == 3 ? .degrees(90) : .degrees(-90)))
             .contentShape(RoundedRectangle(cornerRadius: 8))
@@ -40,13 +51,18 @@ struct TallyButtonView: View {
             .onLongPressGesture(minimumDuration: 1.0, perform: {
                 playerScore -= 1
             })
-        .onRotate($orientation)
+            .onRotate($orientation)
+        }
     }
 }
 
+
+
 struct TallyButton_Previews: PreviewProvider {
     static var previews: some View {
-        TallyButtonView(playerScore: 5, color: .green, playerName: "Shasta")
-//            .previewInterfaceOrientation(.portrait)
+        TallyButtonView(playerScore: 5, color: .green, playerName: "Gregory")
+            .frame(width: 400, height: 400)
+            .previewLayout(.sizeThatFits)
+            .rotationEffect(.degrees(90))
     }
 }
