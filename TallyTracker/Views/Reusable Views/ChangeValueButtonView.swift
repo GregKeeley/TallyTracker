aac58271d7ae183bfdf805d5b17146b88d799bb5
GregKeeley <darkscuall@yahoo.com>
2023-03-16 14:28:26 -0400

    Stylizing Checkpoint.

--- TallyTracker/Views/Reusable Views/ChangeValueButtonView.swift
+++ TallyTracker/Views/Reusable Views/ChangeValueButtonView.swift
@@ -16,9 +16,20 @@ struct ChangeValueButtonView: View {
             ZStack {
                 RoundedRectangle(cornerRadius: 10)
                     .stroke()
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 2)
+//                    .foregroundColor(.red)
+                    .offset(x: 0.5, y: 0.5)
                 HStack {
                     ZStack {
+                        Circle()
+                            .stroke(lineWidth: 2.5)
+                            .foregroundColor(.blue)
+                            .offset(x: 0.5, y: 0.5)
+                            
                         Image(systemName: "minus.circle")
+                            .resizable()
+                            .frame(width: geo.size.height, height: geo.size.height)
                             .foregroundColor(.red)
                     }
                     .onTapGesture {
@@ -29,7 +40,13 @@ struct ChangeValueButtonView: View {
                         .foregroundColor(.red)
                     Spacer()
                     ZStack {
+                        Circle()
+                            .stroke(lineWidth: 2)
+                            .foregroundColor(.red)
+                            .offset(x: 0.5, y: 0.5)
                         Image(systemName: "plus.circle")
+                            .resizable()
+                            .frame(width: geo.size.height, height: geo.size.height)
                             .foregroundColor(.red)
                     }
                     .onTapGesture {
@@ -41,10 +58,12 @@ struct ChangeValueButtonView: View {
     }
 }
 
+
+
 struct ChangeValueButtonView_Previews: PreviewProvider {
     static var previews: some View {
         ChangeValueButtonView(currentValue: 0)
-            .previewLayout(.sizeThatFits)
+//            .previewLayout(.sizeThatFits)
             .frame(width: 80, height: 20)
     }
 }
