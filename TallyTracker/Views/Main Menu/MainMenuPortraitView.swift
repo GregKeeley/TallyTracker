3f3a5a1d728c80756288bb2033bf820e9af3c661
GregKeeley <darkscuall@yahoo.com>
2023-04-17 15:20:18 -0400

    Simplified the MainMenuPortraitView to start preparing it for the major redesign.

--- TallyTracker/Views/Main Menu/MainMenuPortraitView.swift
+++ TallyTracker/Views/Main Menu/MainMenuPortraitView.swift
@@ -23,49 +23,41 @@ struct MainMenuPortraitView: View {
                         .frame(width: 28, height: 28)
                         .padding(.trailing)
                 }
-                Spacer()
-                HStack {
-                    Spacer()
-                    // TODO: Move this ZStack to it's own subview.
-                    ZStack {
-                        AppColors.primaryLightGreen
-                        Text("Start Tallying Your Way To Victory!")
-                            .font(.system(size: 48))
-                            .fontWeight(.bold)
-                            .minimumScaleFactor(0.4)
-                            .frame(width: 300)
-                        RoundedRectangle(cornerRadius: 8.0)
-                            .stroke()
-                            .padding()
-                    }
-                    Spacer()
+                // TODO: Move this ZStack to it's own subview.
+                ZStack {
+                    AppColors.primaryLightGreen
+                        .cornerRadius(8.0)
+                        .padding()
+                    Text("Start Tallying Your Way To Victory!")
+                        .font(.system(size: 48))
+                        .fontWeight(.bold)
+                        .minimumScaleFactor(0.4)
+                        .frame(width: 300)
+                    RoundedRectangle(cornerRadius: 8.0)
+                        .stroke()
+                        .padding()
                 }
-                Spacer()
-                HStack {
-                    VStack {
-                        NavigationLink(destination: GameTallyView(viewModel: GameViewModel(serveLimit: 5,
-                                                                                           scoreLimit: 11,
-                                                                                           firstTeamColor: .red,
-                                                                                           secondTeamColor: .blue,
-                                                                                           firstTeamName: "team1",
-                                                                                           secondTeamName: "team2",
-                                                                                           firstTeamIsServing: true,
-                                                                                           matchLimit: 5,
-                                                                                           teamsAutomaticallySwitchSides: true))) {
-                            MainMenuGameButtonView(gameTitle: "Official Rules", description: "Play to 11. Win by 2. Best of 5.")
-                                .foregroundColor(colorScheme == .dark ? .white : .black)
-                        }
-                        Spacer(minLength: 40)
-                        NavigationLink {
-                            CustomGameSetupView()
-                        } label: {
-                            MainMenuGameButtonView(gameTitle: "House Rules", description: "Custom game.")
-                                .foregroundColor(colorScheme == .dark ? .white : .black)
-                        }
-                    }
-                    .padding()
-//                    .frame(width: UIScreen.main.bounds.size.width * 0.8, height: UIScreen.main.bounds.size.height * 0.3)
+                NavigationLink(destination: GameTallyView(viewModel: GameViewModel(serveLimit: 5,
+                                                                                   scoreLimit: 11,
+                                                                                   firstTeamColor: .red,
+                                                                                   secondTeamColor: .blue,
+                                                                                   firstTeamName: "team1",
+                                                                                   secondTeamName: "team2",
+                                                                                   firstTeamIsServing: true,
+                                                                                   matchLimit: 5,
+                                                                                   teamsAutomaticallySwitchSides: true))) {
+                    MainMenuGameButtonView(gameTitle: "Official Rules", description: "Play to 11. Win by 2. Best of 5.")
+                        .foregroundColor(colorScheme == .dark ? .white : .black)
+                        .padding()
                 }
+                NavigationLink {
+                    CustomGameSetupView()
+                } label: {
+                    MainMenuGameButtonView(gameTitle: "House Rules", description: "Custom game.")
+                        .foregroundColor(colorScheme == .dark ? .white : .black)
+                        .padding()
+                }
+                
                 Spacer()
             }
         }
