9d2c63b71f5e03b5fede9ff7869d1bf6d1d6b066
GregKeeley <darkscuall@yahoo.com>
2023-04-17 12:21:19 -0400

    Added colors from design guide to the AppColors file.

--- TallyTracker/AppColors.swift
+++ TallyTracker/AppColors.swift
@@ -8,5 +8,18 @@
 import SwiftUI
 
 struct AppColors {
-    // Add App colors here.
+    
+    static let primaryGreen = Color.fromHex(hex: "00A651")
+    static let primaryDarkGreen = Color.fromHex(hex: "057039")
+    static let primaryLightGreen = Color.fromHex(hex: "5CF17D")
+    
+    static let neutralBlack1 = Color.fromHex(hex: "272727")
+    static let netutralGray1 = Color.fromHex(hex: "595959")
+    static let neutralGray2 = Color.fromHex(hex: "767676")
+    static let neutralGray3 = Color.fromHex(hex: "C6C6C6")
+    static let neutralGray4 = Color.fromHex(hex: "F6F6F6")
+    
+    static let secondaryWhite1 = Color.fromHex(hex: "838643")
+    static let secondaryWhite2 = Color.fromHex(hex: "909925")
+    
 }
