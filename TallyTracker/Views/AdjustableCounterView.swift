6571a744e8fc14eb4d45cfca752225be031776d4
GregKeeley <darkscuall@yahoo.com>
2023-04-21 12:20:24 -0400

    Converted images to buttons and cleaned up the backgrounds.

--- TallyTracker/Views/AdjustableCounterView.swift
+++ TallyTracker/Views/AdjustableCounterView.swift
@@ -17,20 +17,38 @@ struct AdjustableCounterView: View {
                 .stroke(lineWidth: 4.0)
                 .frame(width: 127.5, height: 40)
             HStack {
-                // TODO: The plus and minus images need to be buttons.
-                Image(systemName: "minus.circle")
-                    .resizable()
-                    .background(AppColors.neutralGray1)
-                    .clipShape(Circle())
-                    .frame(width: 42.5, height: 42.5)
+                Button {
+                    // Decrement current value.
+                } label: {
+                    
+                    ZStack {
+                        Circle()
+                            .foregroundColor(AppColors.neutralGray2)
+                            .frame(width: 40, height: 40)
+                        Image(systemName: "minus.circle")
+                            .resizable()
+                            .clipShape(Circle())
+                        .frame(width: 42.5, height: 42.5)
+                    }
+                }
+                .foregroundColor(.black)
                 Spacer()
                 Text("\(currentValue)")
                 Spacer()
-                Image(systemName: "plus.circle")
-                    .resizable()
-                    .background(AppColors.primaryGreen)
-                    .clipShape(Circle())
-                    .frame(width: 42.5, height: 42.5)
+                Button {
+                    // Increment current value.
+                } label: {
+                    ZStack {
+                        Circle()
+                            .foregroundColor(AppColors.primaryGreen)
+                            .frame(width: 40, height: 40)
+                        Image(systemName: "plus.circle")
+                            .resizable()
+                            .clipShape(Circle())
+                        .frame(width: 42.5, height: 42.5)
+                    }
+                }
+                .foregroundColor(.black)
             }
             .frame(width: 130, height: 40)
         }
@@ -43,6 +61,6 @@ struct AdjustableCounterView: View {
 struct AdjustableCounterView_Previews: PreviewProvider {
     static var previews: some View {
         AdjustableCounterView(currentValue: .constant(0))
-        //            .previewLayout(.sizeThatFits)
+//                    .previewLayout(.sizeThatFits)
     }
 }
